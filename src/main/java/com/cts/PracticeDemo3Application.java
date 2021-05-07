418d3d0d697aaf2a7cba5305a65b4557181fe5f5
906003 <83817600+906003@users.noreply.github.com>
2021-05-07 17:31:39 +0530

    Add files via upload

---
+++ src/main/java/com/cts/PracticeDemo3Application.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeDemo3Application.class, args);
+	}
+
+}
